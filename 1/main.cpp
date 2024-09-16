8f143c260e89330961fd4161b8816b9af5547df8
Kirill <kirill.a.gribov@gmail.com>
2024-09-16 17:13:23 +0300

    написал main

--- 1/main.cpp
+++ 1/main.cpp
@@ -1,13 +1,60 @@
 #include <iostream>
+#include <string>
 
 #include "detail.hpp"
 
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+#define PROMPT "(d) - decode\n(e) - encode\n"
+
+template <typename T>
+void read(const char*, T&);
+
 int
 main() {
-    Detail_info a("a1", "a2", 1);
-    a.print();
-    std::cout << a.encode() << std::endl;
-    Detail_info b("{'id':'b1','name':'b2','count':3}");
-    b.print();
+    char state;
+    Detail_info detail;
+    cout << PROMPT;
+    while (cin >> state) {
+        switch (state) {
+            case 'd': {
+                string str;
+                cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                cout << "json: ";
+                std::getline(cin, str);
+                if (detail.decode(str) == 0) {
+                    detail.print();
+                } else {
+                    cout << "error" << endl;
+                }
+                break;
+            }
+            case 'e': {
+                string id, name;
+                std::size_t count;
+                read("id: ", id);
+                read("name: ", name);
+                read("count: ", count);
+                string result = detail.encode(id, name, count);
+                cout << result << endl;
+                break;
+            }
+            default: {
+                cout << "error" << endl;
+                break;
+            }
+        }
+        cout << PROMPT;
+    }
     return 0;
+}
+
+template <typename T>
+void
+read(const char* prompt, T& var) {
+    cout << prompt;
+    cin >> var;
 }
