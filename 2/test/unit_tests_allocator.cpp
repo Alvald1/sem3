1564aa422a97646cb8751e5e94585626cb4d3841
Kirill <kirill.a.gribov@gmail.com>
2024-10-07 19:22:28 +0300

    написал тесты на копирующий и перемещающий конструкторы и операторы

--- 2/test/unit_tests_allocator.cpp
+++ 2/test/unit_tests_allocator.cpp
@@ -68,7 +68,50 @@ TEST(allocator_resize, invalid_value_negative) {
     EXPECT_THROW(allocator.resize(-5), std::invalid_argument);
 }
 
+// Test the resize function with invalid value overflow
 TEST(allocator_resize, overflow) {
     Allocator allocator(10);
     EXPECT_THROW(allocator.resize(std::numeric_limits<int>::max()), std::overflow_error);
+}
+
+// Test the copy constructor
+TEST(allocator_constructor, copy_constructor) {
+    Allocator allocator(10);
+    Allocator allocator2(allocator);
+    EXPECT_EQ(allocator2.size_, 1);
+    EXPECT_EQ(allocator2.capacity_, 11);
+}
+
+// Test the move constructor
+TEST(allocator_constructor, move_constructor) {
+    Allocator allocator(10);
+    Allocator allocator2(std::move(allocator));
+    EXPECT_EQ(allocator2.size_, 1);
+    EXPECT_EQ(allocator2.capacity_, 11);
+}
+
+// Test the copy assignment operator
+TEST(allocator_operator, copy) {
+    Allocator allocator(10);
+    Allocator allocator2;
+    allocator2 = allocator;
+    EXPECT_EQ(allocator2.size_, 1);
+    EXPECT_EQ(allocator2.capacity_, 11);
+}
+
+// Test the copy assignment operator yourself
+TEST(allocator_operator, copy_yourself) {
+    Allocator allocator(10);
+    allocator = allocator;
+    EXPECT_EQ(allocator.size_, 1);
+    EXPECT_EQ(allocator.capacity_, 11);
+}
+
+// Test the move assignment operator
+TEST(allocator_operator, move) {
+    Allocator allocator(10);
+    Allocator allocator2;
+    allocator2 = std::move(allocator);
+    EXPECT_EQ(allocator2.size_, 1);
+    EXPECT_EQ(allocator2.capacity_, 11);
 }
