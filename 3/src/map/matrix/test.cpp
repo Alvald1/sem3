e3f60c8eb13a50946b19144788d87ad3f3a50b88
Kirill <kirill.a.gribov@gmail.com>
2024-12-07 12:14:08 +0300

    удалил лишние файлы

--- 3/src/map/matrix/test.cpp
+++
@@ -1,8 +0,0 @@
-#include "iterator.hpp"
-
-int
-main() {
-
-    static_assert(std::contiguous_iterator<MatrixIterator<int, false>>);
-    static_assert(std::contiguous_iterator<MatrixIterator<int, true>>);
-}
