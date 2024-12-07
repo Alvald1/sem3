7220e74e3c05a5d5141f962a5c97d8538725bdef
Kirill <kirill.a.gribov@gmail.com>
2024-12-08 00:15:21 +0300

    исправил Summoner

--- 3/src/queue/entity/summoner.hpp
+++ 3/src/queue/entity/summoner.hpp
@@ -2,41 +2,74 @@
 #define SUMMONER_HPP
 
 #include <unordered_map>
-#include "entity.hpp"
 #include "../../schools/school/school.hpp"
-#include "../../level/level.hpp"
+#include "../../schools/schools.hpp"
+#include "entity.hpp"
 
 class Summoner : public Entity {
-private:
+  private:
     size_t energy;
     const size_t max_energy;
     size_t current_experience;
-    double accum_index;
-    std::unordered_map<School, Level> level;
+    size_t accum_index;
+    std::unordered_map<size_t, size_t> level;
 
-public:
-    Summoner(size_t id, const std::string& name, size_t initiative, size_t max_hp,
-             size_t max_energy, double accum_index = 1.0)
-        : Entity(id, name, initiative, max_hp),
-          energy(max_energy),
-          max_energy(max_energy),
-          current_experience(0),
-          accum_index(accum_index) {}
-
-    size_t getEnergy() const { return energy; }
-    const std::unordered_map<School, Level>& getLevel() const { return level; }
-
-    void accum_energy() {
-        energy = std::min(energy + 1, max_energy);
+  public:
+    explicit Summoner(const Ability& ability, size_t max_energy, const Schools& schools, size_t accum_index = 1)
+        : Entity(ability), energy(max_energy), max_energy(max_energy), current_experience(0), accum_index(accum_index) {
+        // Initialize levels for all available schools to 1
+        for (const auto& school : schools.get_schools()) {
+            level[school.get_id()] = 1;
+        }
     }
 
-    void upgrade(School school) {
-        auto it = level.find(school);
-        if (it == level.end()) {
-            level[school] = Level(0, current_experience);
-        } else {
-            level[school].upgrade(current_experience);
+    size_t
+    get_energy() const {
+        return energy;
+    }
+
+    size_t
+    get_experience() const {
+        return current_experience;
+    }
+
+    size_t
+    get_school_level(size_t school_id) const {
+        auto it = level.find(school_id);
+        return it != level.end() ? it->second : 0;
+    }
+
+    bool
+    has_enough_energy(size_t required_energy) const {
+        return energy >= required_energy;
+    }
+
+    void
+    add_experience(size_t exp) {
+        current_experience += exp;
+    }
+
+    void
+    spend_energy(size_t amount) {
+        if (amount > energy) {
+            throw std::runtime_error("Not enough energy");
         }
+        energy -= amount;
+    }
+
+    const std::unordered_map<size_t, size_t>&
+    get_level() const {
+        return level;
+    }
+
+    void
+    accum_energy() {
+        energy = std::min(energy + accum_index, max_energy);
+    }
+
+    void
+    upgrade(size_t school_id) {
+        ++level[school_id];
     }
 };
 
