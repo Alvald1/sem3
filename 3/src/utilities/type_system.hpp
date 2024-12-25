9ea8c3a75eda00aa1826fb4f9c45428a5c13a73c
Kirill <kirill.a.gribov@gmail.com>
2024-12-25 23:34:28 +0300

    упростил систему типов

--- 3/src/utilities/type_system.hpp
+++ 3/src/utilities/type_system.hpp
@@ -7,59 +7,52 @@ enum class TroopType : uint8_t { SUMMONER = 0, MORAL = 1, AMORAL = 2 };
 
 class TypeSystem {
   private:
-    static constexpr uint8_t PRESENCE_MASK = 0x0F; // 0000 1111
-    static constexpr uint8_t SIGN_MASK = 0xF0;     // 1111 0000
-    static constexpr uint8_t SIGN_SHIFT = 4;
-    static constexpr uint8_t TROOP_TYPE_MASK = 0xC0; // 1100 0000
-    static constexpr uint8_t TROOP_TYPE_SHIFT = 6;
+    static constexpr uint8_t DIGITS_PER_EFFECT = 2;
+    static constexpr uint8_t MAX_EFFECTS = 4;
 
   public:
     static void
     setEffect(uint8_t& encoded, EffectType type, bool hasEffect, bool isPositive) {
-        uint8_t effectBit = 1 << static_cast<uint8_t>(type);
-        uint8_t signBit = 1 << (static_cast<uint8_t>(type) + SIGN_SHIFT);
+        uint8_t pos = static_cast<uint8_t>(type) * DIGITS_PER_EFFECT;
+        encoded &= ~(0b11 << pos);
 
         if (hasEffect) {
-            encoded |= effectBit;
+            encoded |= (1 << pos);
             if (isPositive) {
-                encoded |= signBit;
-            } else {
-                encoded &= ~signBit;
+                encoded |= (1 << (pos + 1));
             }
-        } else {
-            encoded &= ~effectBit;
-            encoded &= ~signBit;
         }
     }
 
     static bool
     hasEffect(uint8_t encoded, EffectType type) {
-        return (encoded & (1 << static_cast<uint8_t>(type))) != 0;
+        uint8_t pos = static_cast<uint8_t>(type) * DIGITS_PER_EFFECT;
+        return (encoded >> pos) & 1;
     }
 
     static bool
     isPositive(uint8_t encoded, EffectType type) {
-        return (encoded & (1 << (static_cast<uint8_t>(type) + SIGN_SHIFT))) != 0;
+        uint8_t pos = static_cast<uint8_t>(type) * DIGITS_PER_EFFECT;
+        return (encoded >> (pos + 1)) & 1;
     }
 
     static void
     setTroopType(uint8_t& encoded, TroopType type) {
-        encoded &= ~TROOP_TYPE_MASK; // Clear troop type bits
-        encoded |= (static_cast<uint8_t>(type) << TROOP_TYPE_SHIFT);
+        encoded &= 0b00111111;
+        encoded |= (static_cast<uint8_t>(type) << 6);
     }
 
     static TroopType
     getTroopType(uint8_t encoded) {
-        uint8_t typeValue = (encoded & TROOP_TYPE_MASK) >> TROOP_TYPE_SHIFT;
-        return static_cast<TroopType>(typeValue);
+        return static_cast<TroopType>(encoded >> 6);
     }
 
     static std::vector<std::pair<EffectType, bool>>
     get_effects(uint8_t encoded) {
         std::vector<std::pair<EffectType, bool>> active_effects;
 
-        for (uint8_t i = 0; i <= static_cast<uint8_t>(EffectType::HEALTH); ++i) {
-            EffectType type = static_cast<EffectType>(i);
+        for (uint8_t i = 0; i < MAX_EFFECTS; ++i) {
+            auto type = static_cast<EffectType>(i);
             if (hasEffect(encoded, type)) {
                 active_effects.emplace_back(type, isPositive(encoded, type));
             }
